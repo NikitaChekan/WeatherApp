01dbdd9df71b95a0c856b8b658644cc1ecec60b5
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-02-11 20:27:05 +0300

    work with AlertController

---
+++ WeatherApp/Extensions/ViewController+AlertController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController+AlertController.swift
+//  WeatherApp
+//
+//  Created by Nikita Chekan on 11.02.2023.
+//
+
+import UIKit
+
+extension ViewController {
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        alertController.addTextField { value in
+            let cities = ["Gomel", "Moscow", "Minsk", "Toronto", "Saint Petersburg"]
+            value.placeholder = cities.randomElement()
+        }
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = alertController.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                print("search info for the \(cityName)")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(search)
+        alertController.addAction(cancel)
+        present(alertController, animated: true, completion: nil)
+    }
+}
--- WeatherApp/ViewControllers/ViewController.swift
+++ WeatherApp/ViewControllers/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var weatherIconImageView: UIImageView!
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var temperatureLabel: UILabel!
+    @IBOutlet var feelsLikeTemperatureLabel: UILabel!
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        self.presentSearchAlertController(
+            withTitle: "Enter city name",
+            message: nil,
+            style: .alert
+        )
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
 }
 
